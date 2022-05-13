5675b15f3c3429f5796ec84cc7fbce444daf560a
ikul08 <j.mermoz@absiskey.com>
2022-05-13 16:00:15 +0200

    commit jme

---
+++ testapp/tests/test_jerome.py
@@ -0,0 +1,23 @@
+from ..modules import jerome as jfile
+from ..modules import solenne as sfile
+
+def test_pp():
+    assert jfile.parse_punctuation("mon,;: exemple") == "mon exemple"
+
+def mock_pp(blabla):
+    return ('mon exemple')
+
+def mock_psw(blablabla):
+    return (',,;;')
+
+def test_pt(monkeypatch):
+    monkeypatch.setattr(jfile, "parse_punctuation", mock_pp)
+    monkeypatch.setattr(sfile, "parse_stop_words", mock_psw)
+    assert jfile.parse_text('mon, exemple exemple') == 'mon exemple'
+
+def test_inte():
+    assert jfile.parse_text("mon, test; est: bon bonjour je coucou ") == "mon test est bonjour coucou"
+
+def test_fonctionnel():
+
+    
